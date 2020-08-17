e0adb499a2d5bc4f673b3a659c57a32be8822338
Anthony <anthony.bertnyk@gmail.com>
2020-08-17 22:57:37 +0000

    Add floating origin

---
+++ Assets/OriginController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OriginController : MonoBehaviour
+{
+    int threshold = 5000;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        if(pos.magnitude > threshold)
+        {
+            foreach(GameObject g in SceneManager.GetActiveScene().GetRootGameObjects())
+            {
+                g.transform.position -= pos;
+            }
+        }
+    }
+}
